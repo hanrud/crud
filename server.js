8216c4d4f4fbdd23aaa0735a08cfcbca07a1c862
hania <hania.rudy@gmail.com>
2017-03-26 14:25:32 +0200

    create access point for server

--- server.js
+++ server.js
@@ -1,13 +1,1 @@
-var table = document.getElementById("table");
-var tableHTML = '';
-
-
-
-for (var i = 1; i < 10; i++) {
-  var row = table.insertRow();
-    for(var j = 1; j < 10; j++){
-        var cell1 = row.insertCell();
-        cell1.innerHTML = i * j;
-    }
-
-}
+console.log("Witaj swiecie ");
