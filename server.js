fc7e936e2c0f6209b820030fe55da04431cdbd27
hania <hania.rudy@gmail.com>
2017-03-26 15:43:01 +0200

    create url respose

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.listen(port, ()  => {
+app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`);
+});
+
+app.get('/', (request, response) => {
+    response.send('wszystko ok');
 });
