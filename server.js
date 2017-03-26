11ef64c83aee3bf4a47352e12c590ee06a5116a3
hania <hania.rudy@gmail.com>
2017-03-26 16:47:09 +0200

    add nodemon as dev dep; add simple form; create post on submit

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+console.log("zmiana");
+
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`);
 });
@@ -9,4 +11,8 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
     console.log(__dirname);
     res.sendFile(__dirname + '/index.html')
+});
+
+app.post('/titles', (req, res) => {
+  console.log('Wait for titles');
 });
